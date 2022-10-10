aa6d62eade61176e205cdbbb8ed0f1ddf1f54fbb
sumsung524 <me@xmq.plus>
2022-10-10 16:34:36 +0800

    主题功能搭建完成 版本v1.0.0

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
+# Minimalist-Search-Page
+
+# ![Minimalist-Search-Page](/images/favicon.png)
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg?style=flat-square)](https://github.com/Sumsung524/Minimalist-Search-Page/blob/master/LICENSE) [![version:v1.0.0](https://img.shields.io/badge/version-v1.0.0-green.svg?style=flat-square)](https://github.com/Sumsung524/Minimalist-Search-Page/releases)
+
 极简搜索聚合，还原搜索本质。
 
-一、功能介绍
+[Minimalist-Search-Page](https://github.com/Sumsung524/Minimalist-Search-Page)，一个极简高效的搜索聚合页面。
+
+国外演示地址Demo:[极简搜索-Minimalist Search](https://sumsung524.github.io/Minimalist-Search-Page/index.html)
+
+国内演示地址Demo:[极简搜索-Minimalist Search](https://sumsu_1.gitee.io/minimalist-search-page/index.html)
+
+
+
+## 一、功能介绍
 
 - 简约设计。简单美观的搜索界面，远离广告。
 - 轻快实用。专注搜索，极速访问，还原搜索本质。
@@ -13,14 +27,13 @@
 
 
 
-二、使用说明
+## 二、使用说明
 
 快捷键说明
 
-- 回车：光标未在搜索框，回车直接锁定搜索框内容
-- 回车：光标已在搜索框，回车直接进行搜索
-- Tab：Tab切换搜索引擎
-- 
+- `Enter`键：光标未在搜索框，按`Enter`键直接锁定搜索框内容
+- `Enter`键：光标已在搜索框，按`Enter`键直接进行搜索
+- `Tab`键：`Tab`快速切换搜索引擎
 
 其他交互说明
 
@@ -29,7 +42,63 @@
 
 
 
-三、获取方式
+## 三、获取方式
 
-- 直接获取：https://
-- 自行搭建
+该教程及源码记录项目详细，教程介绍的搭建方式多样，目的是适用于无任何开发经验小白群体。
+
+如果你只想体验或直接使用该项目，可“直接获取”。
+
+### 1.直接获取
+
+直接保存国内演示地址([极简搜索-Minimalist Search](https://sumsung524.github.io/Minimalist-Search-Page/index.html))，并设置为浏览器启动页即可直接使用。
+
+如果你需要：
+
+1. 自行修改或自定义内部一些配置/功能。
+2. 演示访问地址过长，不够简洁，需要自定义搜索页面访问地址。
+3. 嵌入到网站或者其他页面中。
+
+可以下载该项目进行进一步操作
+
+### 2.本地搭建
+
+你也可以下载本项目(项目右上角code→Download ZIP)，全部解压后，将`index.html`创建快捷方式至桌面，直接在本地环境使用。
+
+作为开发者，内部代码带有详细注释，你可以将该项目进行自定义配置。甚至，将该页面上传至远程服务器，通过网址进行访问，将网址设置为浏览器启动页地址，代替那些满是广告带着商业气息的启动页。
+
+
+
+### 3.远程服务搭建
+
+如果该项目能够得到你的喜欢，并且打算长期使用，推荐将该项目上传至gitee国内免费仓库，开启pages服务进行访问。
+
+只需要将下载的项目上传到gitee上根目录，开启pages服务即可进行访问。
+
+> 能在Github上看项目的人相信都会，或者有疑问可搜索gitee上传仓库教程、开启pages服务教程等。
+
+
+
+## 四、后续开发
+
+后续新增功能
+
+- 搜索热词功能，根据用户搜索内容显示相关搜索。
+- 用户选择是否开启天气插件。
+- 记住用户偏好设置，存储上次选择的搜索引擎、是否在新页面打开搜索结果、是否显示搜索热词、是否显示天气等个性化设置。
+- 自定义背景，支持修改默认背景。
+
+
+
+## 五、关于
+
+你有更好的建议，联系作者：me@xmq.plus
+
+如果项目能够帮到你，你可以：
+
+- 帮我点亮这个项目的小星星⭐star，谢谢~
+
+或者你可以：
+
+- 赞赏作者
+
+<img src="https://xmq.plus/medias/reward/alipay.jpg" alt="支付宝" style="zoom:22%;" /><img src="https://xmq.plus/medias/reward/wechat.png" alt="微信" style="zoom:26%;" />
--- js/search.js
+++ js/search.js
@@ -1,79 +1,79 @@
-const searchInfo = [{
-    name: "百度",
-    positionItemsIcon: "-0.4167rem 0",
-    positionIcon: "0 0",
-    positionlogo: "0 0",
-    url: "https://www.baidu.com/s?wd="
-}, {
-    name: "谷歌",
-    positionItemsIcon: "-0.5469rem 0",
-    positionIcon: "-0.2083rem 0",
-    positionlogo: "0 -0.4792rem",
-    url: "https://www.google.com/search?q="
-}, {
-    name: "必应",
-    positionItemsIcon: "-0.4167rem -.5208rem",
-    positionIcon: "0 -0.8333rem",
-    positionlogo: "0 -0.9583rem",
-    url: "https://cn.bing.com/search?q="
-}, {
-    name: "知乎",
-    positionItemsIcon: "-0.5469rem -0.5208rem",
-    positionIcon: "-0.2083rem -0.8333rem",
-    positionlogo: "0 -1.4375rem",
-    url: "https://www.zhihu.com/search?type=content&q="
-}, {
-    name: "GitHub",
-    positionItemsIcon: "-0.4167rem -0.9115rem",
-    positionIcon: "0 -1.4583rem",
-    positionlogo: "0 -1.9167rem",
-    url: "https://github.com/search?utf8=✓&q="
-}, {
-    name: "英译中",
-    positionItemsIcon: "-0.5469rem -0.1302rem",
-    positionIcon: "-0.2083rem -0.2083rem",
-    positionlogo: "0 -2.3958rem",
-    url: "https://translate.google.cn/?sl=en&tl=zh-CN&text="
-}, {
-    name: "B站",
-    positionItemsIcon: "-0.5469rem -0.651rem",
-    positionIcon: "-0.2083rem -1.0417rem",
-    positionlogo: "0 -2.875rem",
-    url: "http://search.bilibili.com/all?keyword="
-}, {
-    name: "优酷",
-    positionItemsIcon: "-0.5469rem -0.7813rem",
-    positionIcon: "-0.2083rem -1.25rem",
-    positionlogo: "0 -3.3542rem",
-    url: "https://so.youku.com/search_video/q_"
-}, {
-    name: "腾讯",
-    positionItemsIcon: "-0.4167rem -0.1302rem",
-    positionIcon: "0 -0.2083rem",
-    positionlogo: "0 -3.8333rem",
-    url: "https://v.qq.com/x/search/?q="
-}, {
-    name: "网易云",
-    positionItemsIcon: "-0.4167rem -0.7813rem",
-    positionIcon: "0 -1.25rem",
-    positionlogo: "0 -4.3125rem",
-    url: "https://music.163.com/#/search/m/?s="
-}, {
-    name: "值得买",
-    positionItemsIcon: "-0.5469rem -0.9115rem",
-    positionIcon: "-0.2083rem -1.4583rem",
-    positionlogo: "0 -4.7917rem",
-    url: "https://search.smzdm.com/?c=home&s="
-}, {
-    name: "京东",
-    positionItemsIcon: "-0.4167rem -0.3906rem",
-    positionIcon: "0 -0.625rem",
-    positionlogo: "0 -5.23rem",
-    url: "http://search.jd.com/Search?keyword="
-}];
-
 // 加载DOM后执行脚本
 window.addEventListener('DOMContentLoaded', function () {
+    // 搜索引擎选项信息
+    const searchInfo = [{
+        name: "百度",
+        positionItemsIcon: "-0.4167rem 0",
+        positionIcon: "0 0",
+        positionlogo: "0 0",
+        url: "https://www.baidu.com/s?wd="
+    }, {
+        name: "谷歌",
+        positionItemsIcon: "-0.5469rem 0",
+        positionIcon: "-0.2083rem 0",
+        positionlogo: "0 -0.4792rem",
+        url: "https://www.google.com/search?q="
+    }, {
+        name: "必应",
+        positionItemsIcon: "-0.4167rem -.5208rem",
+        positionIcon: "0 -0.8333rem",
+        positionlogo: "0 -0.9583rem",
+        url: "https://cn.bing.com/search?q="
+    }, {
+        name: "知乎",
+        positionItemsIcon: "-0.5469rem -0.5208rem",
+        positionIcon: "-0.2083rem -0.8333rem",
+        positionlogo: "0 -1.4375rem",
+        url: "https://www.zhihu.com/search?type=content&q="
+    }, {
+        name: "GitHub",
+        positionItemsIcon: "-0.4167rem -0.9115rem",
+        positionIcon: "0 -1.4583rem",
+        positionlogo: "0 -1.9167rem",
+        url: "https://github.com/search?utf8=✓&q="
+    }, {
+        name: "英译中",
+        positionItemsIcon: "-0.5469rem -0.1302rem",
+        positionIcon: "-0.2083rem -0.2083rem",
+        positionlogo: "0 -2.3958rem",
+        url: "https://translate.google.cn/?sl=en&tl=zh-CN&text="
+    }, {
+        name: "B站",
+        positionItemsIcon: "-0.5469rem -0.651rem",
+        positionIcon: "-0.2083rem -1.0417rem",
+        positionlogo: "0 -2.875rem",
+        url: "http://search.bilibili.com/all?keyword="
+    }, {
+        name: "优酷",
+        positionItemsIcon: "-0.5469rem -0.7813rem",
+        positionIcon: "-0.2083rem -1.25rem",
+        positionlogo: "0 -3.3542rem",
+        url: "https://so.youku.com/search_video/q_"
+    }, {
+        name: "腾讯",
+        positionItemsIcon: "-0.4167rem -0.1302rem",
+        positionIcon: "0 -0.2083rem",
+        positionlogo: "0 -3.8333rem",
+        url: "https://v.qq.com/x/search/?q="
+    }, {
+        name: "网易云",
+        positionItemsIcon: "-0.4167rem -0.7813rem",
+        positionIcon: "0 -1.25rem",
+        positionlogo: "0 -4.3125rem",
+        url: "https://music.163.com/#/search/m/?s="
+    }, {
+        name: "值得买",
+        positionItemsIcon: "-0.5469rem -0.9115rem",
+        positionIcon: "-0.2083rem -1.4583rem",
+        positionlogo: "0 -4.7917rem",
+        url: "https://search.smzdm.com/?c=home&s="
+    }, {
+        name: "京东",
+        positionItemsIcon: "-0.4167rem -0.3906rem",
+        positionIcon: "0 -0.625rem",
+        positionlogo: "0 -5.23rem",
+        url: "http://search.jd.com/Search?keyword="
+    }];
 
     // 获取元素
     const searchBox = document.querySelector('.search-box');
@@ -82,14 +82,13 @@ window.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.querySelector('.search-bar').querySelector('#searchInput');
     const searchBtn = searchBox.querySelector('.search-btn');
     const searchCard = document.querySelector('.search-bar').querySelector('.search-card');
-    var flag = true;
 
-
-    // 一、启动页面聚焦搜索栏
+    // 一、页面启动光标选中搜索栏
     searchInput.select();
 
-    // 二、显示与隐藏搜索引擎选项卡 search-card
-    // 1.点击searchIco显示/隐藏选项卡
+    // 二、显示/隐藏搜索引擎选项卡 search-card
+    // 1.点击输入框左侧图标(searchIco)显示/隐藏选项卡 
+    var flag = true;
     searchIco.addEventListener('click', function (e) {
         e.stopPropagation();    // 阻止事件冒泡，防止触发searchBox点击事件
         if (flag) {
@@ -111,7 +110,7 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     // 三、生成搜索引擎选项
-    // 1.根据搜索引擎数量自动生成searchCard中items信息
+    // 1.根据搜索引擎信息searchInfo自动生成searchCard中items信息
     for (let i = 0; i < searchInfo.length; i++) {
         let li = document.createElement('li');
         li.setAttribute('data-index', i);   // 设置自定义属性index
@@ -122,7 +121,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // 四、切换搜索引擎
     // 1.点击li，改变搜索引擎信息、Ico及Logo    事件委托
-    var num = 0;
+    var num = 0;    // 记录li索引号
     searchCard.addEventListener('click', function (e) {
         let index = e.target.getAttribute('data-index') || e.target.parentNode.getAttribute('data-index');
         // 1.1切换selectIcon
@@ -133,21 +132,19 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     // 五、点击搜索
-    // 在新页面打开选项开关
+    // 1.在新页面打开选项开关
     let newPageOn = true;
     searchBtn.addEventListener('click', function () {
         if (newPageOn) {
-            window.open(searchInfo[num].url + searchInput.value);   // 新窗口打开
-            searchInput.select();   // 搜索后选中
+            window.open(searchInfo[num].url + searchInput.value);       // 新窗口打开
+            searchInput.select();   // 搜索后选中输入文本
         } else {
-            // 当前页面打开
-            // location.assign(searchInfo[num].url + searchInput.value);
-            location.href = searchInfo[num].url + searchInput.value;
+            location.href = searchInfo[num].url + searchInput.value;    // 当前页面打开
         }
     });
 
-    // 六、未聚焦回车聚焦,聚焦回车搜索
-    // 判断聚焦输入栏状态
+    // 六、快捷键
+    // 1.判断聚焦输入栏状态
     var enter = true;
     searchInput.addEventListener('focus', function () {
         enter = true;
@@ -158,12 +155,19 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     window.addEventListener('keydown', function (e) {
+        // 2.回车Enter快捷键
         if (e.code === 'Enter' && enter) {
-            searchBtn.click();
-            console.log('回车搜索');
+            searchBtn.click();      // 聚焦时进行搜索
         } else if (e.code === 'Enter' && !enter) {
-            searchInput.select();
-            console.log('聚焦搜索框');
+            searchInput.select();   // 未聚焦时聚焦搜索栏
+        }
+
+        // 3.Tab快捷键，切换搜索引擎
+        if (e.code === 'Tab') {
+            e.preventDefault();   //阻止默认行为
+            num++;
+            if (num >= searchCard.querySelectorAll('li').length) num = 0;
+            searchCard.querySelectorAll('li')[num].click();
         }
     });
 
